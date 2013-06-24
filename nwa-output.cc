a8ef87026698e4e92a3b1f40aa7c3dcd68778586
Evan Driscoll <driscoll@cs.wisc.edu>
2013-06-24 17:38:01 -0500

    Respect LLVM requirement that FunctionPass doesn't create globals
    Make it a ModulePass instead

--- nwa-output.cc
+++ nwa-output.cc
@@ -16,19 +16,24 @@ using namespace llvm;
 
 namespace  {
 
-    struct Hello : public FunctionPass {
+    struct Hello : public ModulePass {
 
         static char ID;
-        Hello() : FunctionPass(ID) {}
+        Hello() : ModulePass(ID) {}
 
-        virtual bool doInitialization(Module &m) {
-            LLVMContext & context = getGlobalContext();
-            IntegerType * int32 = TypeBuilder<types::i<32>, true>::get(context);
-            m.getOrInsertGlobal("zzzzz", int32);
+        virtual bool runOnModule(Module &m) {
+            for (Module::iterator func = m.begin();
+                 func != m.end(); ++func)
+            {
+                doFunction(m, *func);
+            }
             return false;
         }
 
-        virtual bool runOnFunction(Function &f) {
+        bool doFunction(Module & m, Function & f) {
+            LLVMContext & context = getGlobalContext();
+            IntegerType * int32 = TypeBuilder<types::i<32>, true>::get(context);
+            m.getOrInsertGlobal("zzzzz_" + f.getName().str(), int32);
             return false;
         }
     };  // end of struct Hello
