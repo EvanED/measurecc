6cfdf018ed82141a6a99103609e4ae9d772ee06e
Evan Driscoll <driscoll@cs.wisc.edu>
2013-06-25 13:26:12 -0500

    Make timer.cc a little more interesting

--- timer.cc
+++ timer.cc
@@ -1,14 +1,12 @@
 #include "timer.hpp"
 
 _measurecc::Timer t1,t2;
+static int x = 0;
 
-namespace {
-
-
-    void foo() {
-        t1.start();
-        t1.stop();
-        t1.total_time();
-    }
+void foo() {
+    x=5;
+    t1.start();
+    t1.stop();
+    t1.total_time();
 }
 
